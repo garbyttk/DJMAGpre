85d540f88422f1d63ddfd6913251d6b50897426d
admin <a5816049@aoyama.jp>
2018-07-23 15:41:53 +0900

    add Files

---
+++ polls/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render,redirect
+
+from django.http import HttpResponse
+from .models import Question,Choice
+from django.shortcuts import get_object_or_404
+
+# Create your views here.
+def index(request):
+    return render(request, 'polls/index.html', {
+        'questions': Question.objects.all(),
+    })
+
+def detail(request, pk):
+    obj = get_object_or_404(Question, pk=pk)
+    return render(request, 'polls/detail.html', {
+        'question': obj,
+    })
+
+def vote(request, pk):
+    question = get_object_or_404(Question, pk=pk)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return redirect('poll_results', pk)
+
+def results(request, pk):
+    obj = get_object_or_404(Question, pk=pk)
+    return render(request, 'polls/results.html', {
+        'question': obj,
+    })
